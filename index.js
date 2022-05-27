89b8541b3572ac25a82b3c8c311877cfb4a4ce09
fatihlhz <alfatih.hergun@gmail.com>
2022-05-27 20:14:13 +0700

    Basic js

---
+++ index.js
@@ -0,0 +1,18 @@
+const name = "Fatih";
+const age = 19;
+const isMarried = false;
+const arr = [1,2,3];
+const myObject = {
+    street: "Jl Manunggal",
+    No: 130
+}
+
+const say = (name, age) => {
+    return `Hello my nsme is ${name}, i'am ${age} years old`;
+}
+
+
+
+
+
+console.log(say(name, age));
