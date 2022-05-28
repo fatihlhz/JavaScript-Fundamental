b7d30063000725696eb78932601624ace8887e9e
fatihlhz <alfatih.hergun@gmail.com>
2022-05-28 13:10:15 +0700

    Saturday

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Deklarasi Var
 const name = "Fatih";
 const age = 19;
 const isMarried = false;
@@ -7,12 +8,17 @@ const myObject = {
     No: 130
 }
 
+// Arrow Function
 const say = (name, age) => {
     return `Hello my nsme is ${name}, i'am ${age} years old`;
 }
 
+// Rest Operator
+const count = (...num) => {
+    console.log(...num);
+}
 
-
+count(arr);
 
 
 console.log(say(name, age));
